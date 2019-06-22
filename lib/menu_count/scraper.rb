b0e813fad6d5e0ea7bcd8216ed6dcdaf8e4cb825
Sara Harvey <saraharveynyc@gmail.com>
2019-06-22 08:24:06 -0400

    scraping category and items

---
+++ lib/menu_count/scraper.rb
@@ -0,0 +1,42 @@
+require 'nokogiri'
+require 'open-uri'
+require 'colorize'
+
+class MenuCount::Scraper
+  def initialize
+  end
+
+  def self.scrape_categories
+    page = Nokogiri::HTML(open("https://www.mcdonalds.com/us/en-us/full-menu.html"))
+    categories = []
+
+    page.css(".mcd-category-page__link-item").each do |category|
+      categories << {
+        :name => category.css(".category-title").text,
+        :link => category.css("a").first['href'] }
+      end
+      categories
+  end
+
+  def self.scrape_items(number)
+    link = MenuCount::Category.links[number.to_i - 1]
+
+    page = Nokogiri::HTML(open("https://www.mcdonalds.com" + link))
+    items = []
+
+    page.css(".categories-item-details").each do |item|
+    items << {
+      :item_name => item.css(".categories-item-name").text.strip.blue,
+      :calories => item.css(".categories-item-calories").text.strip}
+    end
+
+    list = items.sort_by {|item_hash| item_hash[:calories]}
+
+    list.each do |sorted_list|
+    puts sorted_list.values.push("\n")
+  end
+  end
+
+end
+
+MenuCount::Scraper.scrape_categories
