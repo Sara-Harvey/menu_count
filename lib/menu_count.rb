b79439124c97ea37b46bdd2ab6aca21e77a027a0
Sara Harvey <saraharveynyc@gmail.com>
2019-07-08 00:02:37 +0000

    module

--- lib/menu_count.rb
+++ lib/menu_count.rb
@@ -1,11 +1,7 @@
-
-class MenuCount
-  
-  def self.start
-    cli = CLI.new
-    cli.see_categories
-  end
-
-end
-
-require './lib/menu_count/cli.rb'
+require_relative "./menu_count/scraper"
+require_relative "./menu_count/category"
+require_relative "./menu_count/version"
+require_relative "./menu_count/cli"
+require "colorize"
+require 'nokogiri'
+require 'open-uri'
